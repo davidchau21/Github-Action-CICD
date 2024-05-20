c19c3d5a148b598500bb84f40fc5c7d971cf4275
davidchau21 <davidduongxu1@gmail.com>
2024-05-20 20:36:45 +0700

    rename test application

--- src/test/java/com/example/githubactioncicd/GitHubActionCICDApplicationTests.java
+++ src/test/java/com/example/githubactioncicd/GitHubActionCICDApplicationTests.java
@@ -1,10 +1,9 @@
 package com.example.githubactioncicd;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyProjectGitHubActionCicdApplicationTests {
+class GitHubActionCICDApplicationTests {
 
 //    @Test
 //    void contextLoads() {
